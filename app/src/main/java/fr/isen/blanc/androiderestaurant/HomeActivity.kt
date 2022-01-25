546655d54996da17693251fe6325cf46e19f438a
Blanc-Numa <numa.blanc@isen.yncrea.fr>
2022-01-25 10:12:59 +0100

    Adding activity and toast

--- app/src/main/java/fr/isen/blanc/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/blanc/androiderestaurant/HomeActivity.kt
@@ -1,11 +1,34 @@
 package fr.isen.blanc.androiderestaurant
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val text = "Bienvenue"
+        val duration = Toast.LENGTH_SHORT
+
+        val toast = Toast.makeText(applicationContext, text, duration)
+        toast.show()
+
+
+        val entreeButton =findViewById<Button>(R.id.starterBtn)
+        entreeButton.setOnClickListener {
+
+            val text = "Menu des entrées"
+            val duration = Toast.LENGTH_SHORT
+
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.show()
+
+            val Intent = Intent(this,starter::class.java)
+            startActivity(Intent)
+        }
     }
 }
+
